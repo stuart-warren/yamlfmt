1d409c964a0a00334fb74fde1aa794fd3d0a1785
stuart.warren <stuart.warren@ocado.com>
2019-10-10 16:05:29 +0100

    test: adds extra doc separator
    FIXME

--- format_test.go
+++ format_test.go
@@ -60,3 +60,50 @@ bar:
 	t.Logf("got:\n%v\n", out)
 	t.Logf("expected:\n%v\n", exp)
 }
+
+func TestYamlMultiSort(t *testing.T) {
+	var in = `---
+k:
+  c: l
+  i:
+    i: k
+  v:
+    # comment
+    c: i
+---
+k:
+  c: l
+  i:
+    i: k
+  v:
+    # comment
+    c: i
+`
+	var expected = `---
+k:
+  c: l
+  i:
+    i: k
+  v:
+    # comment
+	c: i
+---
+k:
+  c: l
+  i:
+    i: k
+  v:
+    # comment
+    c: i
+`
+	exp := []byte(expected)
+	out, err := yamlfmt.Format(bytes.NewBuffer([]byte(in)))
+	if err != nil {
+		t.Fatalf("Unexpected error: %s\n", err)
+	}
+	if !bytes.Equal(out, exp) {
+		t.Fatalf("Got:\n%s\nexpected:\n%s\n", out, exp)
+	}
+	t.Logf("got:\n%v\n", out)
+	t.Logf("expected:\n%v\n", exp)
+}
