261e9fabd3fa46eca7f39e6890f4fcf3227211b9
Stuart Warren <stuartwarren@ntlworld.com>
2019-10-10 21:31:01 +0100

    fix: resolve issue with extra doc separators
    fixes #11

--- format_test.go
+++ format_test.go
@@ -27,7 +27,7 @@ k:
     c: i
 `
 	exp := []byte(expected)
-	out, err := yamlfmt.Format(bytes.NewBuffer([]byte(in)))
+	out, err := yamlfmt.Format(bytes.NewReader([]byte(in)))
 	if err != nil {
 		t.Fatalf("Unexpected error: %s\n", err)
 	}
@@ -50,7 +50,7 @@ bar:
   foo: baz # comment
 `
 	exp := []byte(expected)
-	out, err := yamlfmt.Format(bytes.NewBuffer([]byte(in)))
+	out, err := yamlfmt.Format(bytes.NewReader([]byte(in)))
 	if err != nil {
 		t.Fatalf("Unexpected error: %s\n", err)
 	}
@@ -86,7 +86,7 @@ k:
     i: k
   v:
     # comment
-	c: i
+    c: i
 ---
 k:
   c: l
@@ -97,7 +97,7 @@ k:
     c: i
 `
 	exp := []byte(expected)
-	out, err := yamlfmt.Format(bytes.NewBuffer([]byte(in)))
+	out, err := yamlfmt.Format(bytes.NewReader([]byte(in)))
 	if err != nil {
 		t.Fatalf("Unexpected error: %s\n", err)
 	}
